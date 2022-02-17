a261c55a85213eeb4a785796c5e7341774912f8a
VITALYALGOSEC <96118544+VITALYALGOSEC@users.noreply.github.com>
2022-02-17 15:05:05 +0200

    Update main.tf

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "devsecops_test" {
     from_port   = 9876
     to_port     = 9876
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
     description = "Outgoing traffic"
   }
   tags = {
