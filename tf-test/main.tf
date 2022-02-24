24c793c2bfde389c769eeddaff9d0df64ef572ab
vitaly.landa <vitaly.landa@algosec.com>
2022-02-24 09:12:29 +0200

    complete great feature

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -61,20 +61,26 @@ restrictions {
 
 resource "aws_security_group" "devsecops_test" {
   name        = "devsecops_test"
-  description = "Display devsecops"
+  description = "Display devsecops "
   vpc_id      = "vpc-12345678"
   ingress {
     from_port   = 8080
     to_port     = 8080
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["172.31.0.0/16"]
     description = "web app"
   }
-  
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "web app"
+  }
   egress {
-    from_port   = 7654
-    to_port     = 7654
-    protocol    = "udp"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["10.20.30.0/24"]
   }
   tags = {
