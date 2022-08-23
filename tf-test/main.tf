fdfaaa0ce8ed3e3ec61f833858f27fd352e50e4a
alonnalgo <97066034+alonnalgo@users.noreply.github.com>
2022-08-23 11:12:08 +0300

    Update main.tf

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -73,9 +73,9 @@ resource "aws_security_group" "devsecops_test" {
   
   egress {
     from_port   = 7654
-    to_port     = 7655
-    protocol    = "tcp"
-    cidr_blocks = ["10.20.30.0/24"]
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "cool_application"
