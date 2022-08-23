1dd86a7b2e2d862fc5ed5a51ee4f5f7fe75b1096
alonnalgo <97066034+alonnalgo@users.noreply.github.com>
2022-08-23 18:03:58 +0300

    Update main.tf

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -65,7 +65,7 @@ resource "aws_security_group" "devsecops_test" {
   vpc_id      = "vpc-12345678"
   ingress {
     from_port   = 0
-    to_port     = 8082
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     description = "web app"
