34ffe79bf7cc62fbd584eb4d152cf2b362869885
alonnalgo <97066034+alonnalgo@users.noreply.github.com>
2022-08-24 15:35:09 +0300

    Update main.tf

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -66,8 +66,8 @@ resource "aws_security_group" "devsecops_test" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["172.31.0.0/16"]
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "web app"
   }
   
