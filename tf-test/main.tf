00aecf98b71de74e861f0a4154798fb72c199a71
VITALYALGOSEC <96118544+VITALYALGOSEC@users.noreply.github.com>
2022-09-15 15:23:12 +0300

    Update main.tf

--- tf-test/main.tf
+++ tf-test/main.tf
@@ -130,6 +130,14 @@ resource "aws_security_group" "AutoSecurityGroupSSHBase" {
     protocol    = "tcp"
     to_port     = "22"
   }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups  = ["sg-0b132d6143ddd6435"]
+
+  }
   egress {
     cidr_blocks = [
       "0.0.0.0/0"
